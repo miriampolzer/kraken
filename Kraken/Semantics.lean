1452680a5c53029c1db3fb039a557c428dcd485c
Andres Erbsen <andres-github@andres.systems>
2026-04-02 11:18:44 -0700

    Remove defaults of flags; move StatusFromResultFlags

--- Kraken/Semantics.lean
+++ Kraken/Semantics.lean
@@ -97,17 +97,11 @@ def Reg64s.set (s : Reg64s) (r : Reg w) (v : w.type) : Reg64s := match r with
     s.set64 r.base (old.replaceLow (BitVec.append v (s.get (.low r.base .W8))))
 
 structure StatusFlags where
-  cf : Bool := false
-  pf : Bool := false
-  af : Bool := false
-  zf : Bool := false
-  sf : Bool := false
-  of : Bool := false
-  deriving Repr, BEq, DecidableEq
-
-structure StatusFromResultFlags where
   cf : Bool
+  pf : Bool
   af : Bool
+  zf : Bool
+  sf : Bool
   of : Bool
   deriving Repr, BEq, DecidableEq
 
@@ -115,7 +109,7 @@ abbrev DataMem := Std.ExtHashMap UInt64 UInt64 -- 8-byte-aligned acceses only no
 instance : Repr DataMem where reprPrec _ _ := "<opaque memory>"
 structure MachineData where -- does not include code or program position
   regs : Reg64s := {}
-  status : StatusFlags := {}
+  status : StatusFlags := .mk false false false false false false
   dmem : DataMem := ∅
   deriving Repr, BEq, DecidableEq
 
@@ -367,7 +361,13 @@ inductive Operation (w : Width)
   | nopalign (alignment : Nat) (pad : Option Nat)
 deriving Repr, DecidableEq
 
-def StatusFlags.from_result {w} (result : BitVec w) (f : StatusFromResultFlags) : StatusFlags :=
+structure StatusFlags.from_result.Remaining where
+  cf : Bool
+  af : Bool
+  of : Bool
+  deriving Repr, BEq, DecidableEq
+
+def StatusFlags.from_result {w} (result : BitVec w) (f : from_result.Remaining) : StatusFlags :=
   { pf := (result.truncate 8).cpop % 2 != BitVec.zero _
     zf := result == BitVec.zero _
     sf := result.msb, cf := f.cf, af := f.af, of := f.of }
